9449f4f4d19711726101a67567be60cbdfe9c7ef
Dmitriy1892 <kodim1892@gmail.com>
2020-09-28 22:54:11 +0300

    Initial commit

---
+++ app/src/main/java/com/bignerdranch/geoquiz/TrueFalse.java
@@ -0,0 +1,27 @@
+package com.bignerdranch.geoquiz;
+
+public class TrueFalse {
+    private int mQuestion;
+    private boolean mTrueQuestion;
+
+    public TrueFalse(int question, boolean trueQuestion) {
+        mQuestion = question;
+        mTrueQuestion = trueQuestion;
+    }
+
+    public int getQuestion() {
+        return mQuestion;
+    }
+
+    public void setQuestion(int mQuestion) {
+        this.mQuestion = mQuestion;
+    }
+
+    public boolean isTrueQuestion() {
+        return mTrueQuestion;
+    }
+
+    public void setTrueQuestion(boolean mTrueQuestion) {
+        this.mTrueQuestion = mTrueQuestion;
+    }
+}
