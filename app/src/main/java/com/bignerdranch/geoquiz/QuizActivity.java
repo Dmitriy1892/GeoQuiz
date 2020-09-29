3d00b9a887d707fdd930d33c034a48f52c709bae
Dmitriy1892 <kodim1892@gmail.com>
2020-09-29 11:38:19 +0300

    TrueFalse refactored to Question.java, toasts updated (displayed at the top)

--- app/src/main/java/com/bignerdranch/geoquiz/QuizActivity.java
+++ app/src/main/java/com/bignerdranch/geoquiz/QuizActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -18,12 +19,13 @@ public class QuizActivity extends AppCompatActivity {
     private Button mPrevButton;
     private TextView mQuestionTextView;
 
-    private TrueFalse[] mQuestionBank = new TrueFalse[] {
-            new TrueFalse(R.string.question_oceans, true),
-            new TrueFalse(R.string.question_mideast, false),
-            new TrueFalse(R.string.question_africa, false),
-            new TrueFalse(R.string.question_americas, true),
-            new TrueFalse(R.string.question_asia, true)
+    private Question[] mQuestionBank = new Question[] {
+            new Question(R.string.question_oceans, true),
+            new Question(R.string.question_mideast, false),
+            new Question(R.string.question_africa, false),
+            new Question(R.string.question_americas, true),
+            new Question(R.string.question_asia, true),
+            new Question(R.string.question_australia, true)
     };
 
     private int mCurrentIndex = 0;
@@ -41,7 +43,7 @@ public class QuizActivity extends AppCompatActivity {
         setContentView(R.layout.activity_quiz);
 
         mQuestionTextView = (TextView)findViewById(R.id.question_text_view);
-        mQuestionTextView.setText(mQuestionBank[mCurrentIndex].getQuestion());
+        mQuestionTextView.setText(mQuestionBank[mCurrentIndex].geTextResId());
         mQuestionTextView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -120,17 +122,23 @@ public class QuizActivity extends AppCompatActivity {
     }
 
     private void updateQuestion() {
-        int question = mQuestionBank[mCurrentIndex].getQuestion();
+        int question = mQuestionBank[mCurrentIndex].geTextResId();
         mQuestionTextView.setText(question);
     }
 
     private void checkAnswer(Boolean userPressedTrue) {
-        if (userPressedTrue == mQuestionBank[mCurrentIndex].isTrueQuestion()) {
-            Toast.makeText(QuizActivity.this, R.string.correct_toast,
-                    Toast.LENGTH_SHORT).show();
+        if (userPressedTrue == mQuestionBank[mCurrentIndex].isAnswerTrue()) {
+            Toast toast = Toast.makeText(QuizActivity.this,R.string.correct_toast, Toast.LENGTH_SHORT);
+            toast.setGravity(Gravity.TOP, 0, 300);
+            toast.show();
+            //Toast.makeText(QuizActivity.this, R.string.correct_toast,
+             //       Toast.LENGTH_SHORT).show();
         } else {
-            Toast.makeText(QuizActivity.this, R.string.incorrect_toast,
-                    Toast.LENGTH_SHORT).show();
+            Toast toast = Toast.makeText(QuizActivity.this,R.string.incorrect_toast, Toast.LENGTH_SHORT);
+            toast.setGravity(Gravity.TOP, 0, 300);
+            toast.show();
+            //Toast.makeText(QuizActivity.this, R.string.incorrect_toast,
+              //      Toast.LENGTH_SHORT).show();
         }
 
     }
